46ff5df4a7370309480972d7127b895cf0d514ec
heguiqin <guiqin_he@163.com>
2019-06-05 10:23:08 +0800

    ok

--- LL_Query.aspx.cs
+++ LL_Query.aspx.cs
@@ -126,7 +126,7 @@ public partial class LL_Query : System.Web.UI.Page
         string sqlstr = @"select qad_pono+'_'+cast(po_dtl.rowid as varchar) as wlh,wlmc,wlms,substring(wltype,1,charindex('-',wltype)-1)line,isnull(rec_Quantity,0)rec_Quantity
                                       from [172.16.5.26].mes.dbo.PUR_PO_Dtl_Form  po_dtl join [172.16.5.26].mes.dbo.PUR_PR_Dtl_Form pr_dtl on  po_dtl.PRNo=pr_dtl.PRNo and po_dtl.PRRowId=pr_dtl.rowid
                                      left join paperless_NoMaterial_Qty  nom on qad_pono+'_'+cast(po_dtl.rowid as varchar)=nom.part
-                                      where  isnull(wlh,'')='无' and  substring(wltype,1,charindex('-',wltype)-1)='" + djtype + "'   and charindex('-',wltype)>0 and flag_qad='是' ";
+                                      where  isnull(wlh,'')='无' and  substring(wltype,1,charindex('-',wltype)-1)='" + djtype + "'   and charindex('-',wltype)>0 and flag_qad='是'   and isnull(po_dtl.qad_pono,'')<>''";
         DataTable xt_dt = SQLHelper.reDs(sqlstr.ToString()).Tables[0];
         try
         {
